f87d4115640fc6def449081cc72a4b5be1783a83
Alejandro <saul.ludena@unl.edu.ec>
2024-05-11 15:45:56 -0500

    actualizando con main

--- pis-5to/controllers/utils/errors.py
+++ pis-5to/controllers/utils/errors.py
@@ -1,6 +1,5 @@
 class Errors:
     error = {
-<<<<<<< HEAD
         '-1': "NO estan las fechas correctas",
         '-2': "Este correo ya está en uso",
         '-3': "Este correo no es válido",
@@ -15,14 +14,14 @@ class Errors:
         '-13': "No existe cuenta",
         '-14': "No tiene el token",
         '-15': "Token no valido",
-=======
-            "-1": "No se encuentra",
-             "-2": "Correo repetido",
-             "-3": "No hay motivos que guardar",
-             "-4": "Cuenta o contraseña no coincidei",
-             "-5": "No hay token",
-             "-6": "Token no válido",
-             "-7": "DNI repetido",
-             "-8": "Longitud de DNI mayor a 10"
->>>>>>> alejandro
+
+
+        "-1": "No se encuentra",
+        "-2": "Correo repetido",
+        "-3": "No hay motivos que guardar",
+        "-4": "Cuenta o contraseña no coincidei",
+        "-5": "No hay token",
+        "-6": "Token no válido",
+        "-7": "DNI repetido",
+        "-8": "Longitud de DNI mayor a 10"
     }
