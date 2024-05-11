1a1d32831e1915caebfc559fac082fe4561f8f81
luisquirola04 <132861783+luisquirola04@users.noreply.github.com>
2024-05-11 15:51:53 -0500

    Update errors.py

--- pis-5to/controllers/utils/errors.py
+++ pis-5to/controllers/utils/errors.py
@@ -14,15 +14,5 @@ class Errors:
         '-12': "No existe el sensor solicitado",
         '-13': "No existe cuenta",
         '-14': "No tiene el token",
-        '-15': "Token no valido",
-=======
-            "-1": "No se encuentra",
-             "-2": "Correo repetido",
-             "-3": "No hay motivos que guardar",
-             "-4": "Cuenta o contraseña no coincidei",
-             "-5": "No hay token",
-             "-6": "Token no válido",
-             "-7": "DNI repetido",
-             "-8": "Longitud de DNI mayor a 10"
->>>>>>> alejandro
+        '-15': "Token no valido"
     }
