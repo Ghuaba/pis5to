9367b9c04b6a8c5d46358010b096c0c8bf902136
guaba <pool.ochoa.robalino@gmail.com>
2024-05-12 13:29:01 -0500

    Implementacion criterios de busqueda para monitorings

--- pis-5to/routes/route_monitoring.py
+++ pis-5to/routes/route_monitoring.py
@@ -3,13 +3,16 @@ from flask import Blueprint, jsonify, make_response, request
 from flask_expects_json import expects_json
 from controllers.utils.errors import Errors
 from controllers.controller_monitoring import ControllerMonitoring
-from .schemas.schemas_monitoring import schema_save
+from .schemas.schemas_monitoring import schema
+from datetime import datetime
+
 
 url_monitoring = Blueprint('url_monitoring', __name__)
 
 
 monitoringC = ControllerMonitoring()
 
+#sirve para listar todos los monitores existentes 
 @url_monitoring.route('/monitoring/list')
 def listMonitoring():
     return make_response(
@@ -19,8 +22,43 @@ def listMonitoring():
     )
 
 
+#sirve para listar monitoreos con base a un intervalo de fecha 
+@url_monitoring.route('/monitoring/list/date', methods=['GET'])
+def list_monitoring_within_date_range():
+    data = request.json
+    
+    if data and 'start_date' in data and 'end_date' in data:
+        start_date = datetime.strptime(data['start_date'], "%Y-%m-%d")
+        end_date = datetime.strptime(data['end_date'], "%Y-%m-%d")
+        
+        controller = ControllerMonitoring()
+        monitoring_within_range = controller.list_within_date_range(data)
+        return make_response(jsonify({"msg": "OK", "code": 200, "datos": [monitoring.serialize for monitoring in monitoring_within_range]}), 200)
+    else:
+        return make_response(
+            jsonify({"msg": "ERROR", "code": 400, "datos": {"error": Errors.error.get(str(m))}}),
+            400
+        )
+
+
+#sirve para listar monitoreos con base a un intervalo de fecha y ademas un iteravalo de coordenadas 
+@url_monitoring.route('/monitoring/list/date/location', methods=['GET'])
+def list_monitoring_within_date_and_location():
+    data = request.json
+    
+    if data and 'start_date' in data and 'end_date' in data:
+        controller = ControllerMonitoring()
+        monitoring_within_range = controller.list_within_date_range(data)
+        return make_response(jsonify({"msg": "OK", "code": 200, "datos": [monitoring.serialize for monitoring in monitoring_within_range]}), 200)
+    else:
+        return make_response(
+            jsonify({"msg": "ERROR", "code": 400, "datos": {"error": Errors.error.get(str(m))}}),
+            400
+        )
+
+#sirve para guardar monitoreso
 @url_monitoring.route('/monitoring/save', methods = ["POST"])
-@expects_json(schema_save)
+@expects_json(schema)
 def saveMonitoring():
     data = request.json  # Supongamos que recibes los datos en formato JSON
     m = monitoringC.save(data)
@@ -34,4 +72,22 @@ def saveMonitoring():
             jsonify({"msg": "ERROR", "code": 400, "datos": {"error": Errors.error.get(str(m))}}),
             400
         )
-
+    
+#sirve para modifiacar monitreos ya existentes
+@url_monitoring.route('/monitoring/modify/<uid>', methods=["POST"])
+@expects_json(schema)
+def modifyMonitoring(uid):
+    data = request.json
+    result = monitoringC.modify(uid, data)
+    
+    if result >= 0:
+        return make_response(
+            jsonify({"msg": "OK", "code": 200, "datos": {"uid": uid}}),
+            200
+        )
+    else:
+        error_message = Errors.error.get(str(result))
+        return make_response(
+            jsonify({"msg": "ERROR", "code": 400, "datos": {"error": error_message}}),
+            400
+        )
